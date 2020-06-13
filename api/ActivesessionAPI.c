6fd0779d8cdd7e6fc7c5ff3638c3988ea9a13495
Ian Lord <lordi@ezmax.ca>
2020-06-13 01:54:05 -0400

    Initial Commit

---
+++ api/ActivesessionAPI.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "ActivesessionAPI.h"
+
+
+#define MAX_BUFFER_LENGTH 4096
+#define intToStr(dst, src) \
+    do {\
+    char dst[256];\
+    snprintf(dst, 256, "%ld", (long int)(src));\
+}while(0)
+
+
+// Get Current Activesession
+//
+// Todo Description
+//
+void
+ActivesessionAPI_getCurrent(apiClient_t *apiClient)
+{
+    list_t    *localVarQueryParameters = NULL;
+    list_t    *localVarHeaderParameters = NULL;
+    list_t    *localVarFormParameters = NULL;
+    list_t *localVarHeaderType = list_create();
+    list_t *localVarContentType = NULL;
+    char      *localVarBodyParameters = NULL;
+
+    // create the path
+    long sizeOfPath = strlen("/1/object/activesession/getCurrent")+1;
+    char *localVarPath = malloc(sizeOfPath);
+    snprintf(localVarPath, sizeOfPath, "/1/object/activesession/getCurrent");
+
+
+
+    list_addElement(localVarHeaderType,"application/json"); //produces
+    apiClient_invoke(apiClient,
+                    localVarPath,
+                    localVarQueryParameters,
+                    localVarHeaderParameters,
+                    localVarFormParameters,
+                    localVarHeaderType,
+                    localVarContentType,
+                    localVarBodyParameters,
+                    "GET");
+
+    if (apiClient->response_code == 200) {
+        printf("%s\n","OK");
+    }
+    //No return type
+end:
+    if (apiClient->dataReceived) {
+        free(apiClient->dataReceived);
+        apiClient->dataReceived = NULL;
+        apiClient->dataReceivedLen = 0;
+    }
+    
+    
+    
+    list_free(localVarHeaderType);
+    
+    free(localVarPath);
+
+}
+
---
+++ api/ActivesessionAPI.h
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../include/apiClient.h"
+#include "../include/list.h"
+#include "../external/cJSON.h"
+#include "../include/keyValuePair.h"
+#include "../include/binary.h"
+
+
+// Get Current Activesession
+//
+// Todo Description
+//
+void
+ActivesessionAPI_getCurrent(apiClient_t *apiClient);
+
+
